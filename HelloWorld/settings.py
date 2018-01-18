0df2307d38cec2a3a06fc74ab11e4a06664de537
AmilcarCH <ingamilcarcaceres@gmail.com>
2018-01-18 08:53:31 -0500

    configuracion con db mysql

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'HelloWorld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
