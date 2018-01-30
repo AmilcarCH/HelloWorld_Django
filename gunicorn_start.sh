88621dfc030863439049fe26009f9a443a6519f6
AmilcarCH <ingamilcarcaceres@gmail.com>
2018-01-30 11:38:23 -0500

    xdxd xd xd

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -11,7 +11,7 @@ echo "Starting $NAME as `whoami`"
 # Activate the virtual environment
 
 cd $DJANGODIR
-source /var/lib/jenkins/workspace/holamundo/bin/activate
+source /var/lib/jenkins/workspace/prueba/holamundo/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
