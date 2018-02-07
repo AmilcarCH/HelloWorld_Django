a9cf6b0714b89a1093311f51eec57d126d46e0f0
AmilcarCH <ingamilcarcaceres@gmail.com>
2018-02-07 08:47:37 -0500

    modificando settings

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8_w_xd9+at6!oq$h1=q$a#xy7-$yt85h0%3jj74gi@#zm)5(_j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [127.0.0.1, 'localhost']
 
 
 # Application definition
